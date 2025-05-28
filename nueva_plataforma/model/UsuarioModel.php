afcc4e4d9f906a9258f3cfde0572635a5ffeeefb
jarmandoR <jose523a@gmail.com>
2025-05-27 21:50:48 -0500

    wip:mostrar consulta

--- nueva_plataforma/model/UsuarioModel.php
+++ nueva_plataforma/model/UsuarioModel.php
@@ -22,7 +22,7 @@ class UsuarioModel {
         }
 
         $sql .= " ORDER BY usu_nombre ASC";
-file_put_contents("debug_sql.log", $sql . PHP_EOL, FILE_APPEND);
+        echo "Consulta SQL: " . $sql;
         $result = $this->db->query($sql);
         return $result ? $result->fetch_all(MYSQLI_ASSOC) : [];
     }
