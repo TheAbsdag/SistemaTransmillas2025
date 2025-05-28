931291d88c3a7a2f400ce79539f6904d47d57468
jarmandoR <jose523a@gmail.com>
2025-05-27 21:36:16 -0500

    wip: nuevo usuarios

--- nueva_plataforma/view/usuarios/index.php
+++ nueva_plataforma/view/usuarios/index.php
@@ -49,9 +49,7 @@
   </table>
 </div>
 
-<script src="../../assets/bootstrap/bootstrap.bundle.min.js"></script>
-<script src="../../assets/datatables/datatables.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <script>
 $(document).ready(function () {
   const tabla = $('#tablaUsuarios').DataTable({
