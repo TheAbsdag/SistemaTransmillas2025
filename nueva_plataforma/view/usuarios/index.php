8e35e31ecac666b388e691872c39fdd93c2b38dd
jarmandoR <jose523a@gmail.com>
2025-05-27 21:34:08 -0500

    wip: nuevo usuarios

--- nueva_plataforma/view/usuarios/index.php
+++ nueva_plataforma/view/usuarios/index.php
@@ -3,8 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <title>Usuarios</title>
-  <link rel="stylesheet" href="../../assets/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="../../assets/datatables/datatables.min.css">
+ <!-- ✅ Bootstrap 5 desde CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- ✅ DataTables desde CDN -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 </head>
 <body>
 <div class="container mt-4">
