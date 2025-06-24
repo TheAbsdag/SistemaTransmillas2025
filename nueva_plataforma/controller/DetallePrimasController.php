42fb22a8f5af0a1de473aa6b31ebcbd3850c72f8
jarmandoR <jose523a@gmail.com>
2025-06-23 21:29:08 -0500

    wip: Nuevo modulo de primas

--- nueva_plataforma/controller/DetallePrimasController.php
+++ nueva_plataforma/controller/DetallePrimasController.php
@@ -2,8 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once 'config/database.php';
-require_once 'model/DetallePrimasModel.php';
+require_once '../config/database.php';
+require_once '../model/DetallePrimasModel.php';
 
 class DetallePrimasController {
     public function detalle() {
--- nueva_plataforma/view/primas/detalle.php
+++ nueva_plataforma/view/primas/detalle.php
@@ -2,7 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once 'controller/DetallePrimasController.php';
+// require_once '../../controller/DetallePrimasController.php';
+
 $sedes = $this->obtenerSedes($db);
 ?>
 <!DOCTYPE html>
