88a183413bf74cfbd5ee216b48ded338ff4a5914
jarmandoR <jose523a@gmail.com>
2025-06-24 22:36:24 -0500

    wip: se modifica desprendible de pago de prima

--- desprendible_primo.php
+++ desprendible_primo.php
@@ -2,41 +2,39 @@
 
 
 $diastrabajados=$_GET["diastrabajados"];
-$sueldo=$_GET["sueldo"];
-$auxilitrans=$_GET["auxilitrans"];
-$pagdiasinca=$_GET["pagdiasinca"];
+// $sueldo=$_GET["sueldo"];
+// $auxilitrans=$_GET["auxilitrans"];
+// $pagdiasinca=$_GET["pagdiasinca"];
 $totaldeveng=$_GET["totaldeveng"];
-$salud=$_GET["salud"];
-$pension=$_GET["pension"];
-$prestamos=$_GET["prestamos"];
-$totaldeduccion=$_GET["totaldeduccion"];
+// $salud=$_GET["salud"];
+// $pension=$_GET["pension"];
+// $prestamos=$_GET["prestamos"];
+// $totaldeduccion=$_GET["totaldeduccion"];
 
-$valor=$_GET["valor"];
-$deudas=$_GET["deudas"];
+// $valor=$_GET["valor"];
+// $deudas=$_GET["deudas"];sueldo
 $validado=$_GET["confirmado"];
-$diasIncapacidad=$_GET["diasIncapacidad"];
-$valordiasVacaciones=$_GET["vacaciones"];
-$diasVacaciones=$_GET["diasvacaciones"];
+// $diasIncapacidad=$_GET["diasIncapacidad"];
+// $valordiasVacaciones=$_GET["vacaciones"];
+// $diasVacaciones=$_GET["diasvacaciones"];
 $firma=$_GET["firma"];
 
-$descriprestamos=$_GET["descriprestamos"];
-$valorAjusteB=$_GET["valorAjuste"];
-$tipoAjusteB=$_GET["tipoAjuste"];
-$descripcionAjusteB=$_GET["descripcionAjuste"];
+// $descriprestamos=$_GET["descriprestamos"];
+// $valorAjusteB=$_GET["valorAjuste"];
+// $tipoAjusteB=$_GET["tipoAjuste"];
+// $descripcionAjusteB=$_GET["descripcionAjuste"];
 $sueldobasico=$_GET["sueldobasico"];
 $transporte=$_GET["transporte"];
 
-$ajustessumB=0;
-$ajustesresB=0;
-if ($tipoAjusteB=="suma") {
-    $ajustessumB=$valorAjusteB;
-    $ajustesresB=0;
-}else if($tipoAjusteB=="descuento"){
-    $ajustessumB=0;
-    $ajustesresB=$valorAjusteB;
-}
-
-// desprendibleBasico.php?cedula=".$rw1[5]."&nombre=".$rw1[1].$rw1[2]."&cargo=$rw1[3]&fechaini=$fechaactual&fechafin=$fechafinal&cuenta=&diastrabajados=$diassitrabajo&sueldo=$valordediastrabajados&auxilitrans=$totalauxilio&pagdiasinca=$valorDiasIncapadidad&totaldeveng=$totaldevengado&salud=$valorSalud&pension=$valorPension&prestamos=$restaABasico&totaldeduccion=$totaldeduccion"
+// $ajustessumB=0;
+// $ajustesresB=0;
+// if ($tipoAjusteB=="suma") {
+//     $ajustessumB=$valorAjusteB;
+//     $ajustesresB=0;
+// }else if($tipoAjusteB=="descuento"){
+//     $ajustessumB=0;
+//     $ajustesresB=$valorAjusteB;
+// }
 
 $fechaini = strtotime($fechaini);
 $fechafin = strtotime($fechafin);
@@ -49,7 +47,6 @@ $fechainiaño=date("Y",$fechaini);
 $fechafindia=date("d",$fechafin);
 $fechafinmes=date("m",$fechafin);
 $fechafinaño=date("Y",$fechafin);   
-//             $fechadelregidia = date("d",$fechadelregi);
 
 // // error_reporting(0);
 require('fpdf/fpdf.php');
@@ -167,9 +164,7 @@ function Footer()
     $this->SetY(-1);
     // Times italic 8
     $this->SetFont('Times','I',8);
-    // $this->Image('assets/piedepaginaberm.jpg',0,260,0);
-    // Número de página
-    // $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+
 }
 }
 
@@ -196,32 +191,7 @@ $pdf->Rect(10, 72, 95, 15); // Dibuja un rectángulo con relleno
 $pdf->SetLineWidth(0.5); // Establece el ancho de línea
 $pdf->SetDrawColor(0, 0, 0); // Establece el color del borde (negro en este caso)
 $pdf->Rect(105, 72, 95,15); // Dibuja un rectángulo sin relleno
-
-
-        //   // Definir borde negro alrededor del encabezado
-        //   $pdf->SetLineWidth(0.2); // Establece el ancho de línea
-        //   $pdf->SetDrawColor(0, 0, 0); // Establece el color del borde (negro)
-        //   $pdf->Rect(12, 90, 185, 100, 'D'); // Dibuja un rectángulo con borde
-
-
-            //         // Dibuja el primer rectángulo
-            // $pdf->SetLineWidth(0.2); // Establece el ancho de línea
-            // $pdf->SetDrawColor(0, 0, 0); // Establece el color del borde (negro en este caso)
-            // $pdf->Rect(12, 90, 92, 10); // Dibuja un rectángulo con relleno
-
-
-            // // Dibuja el segundo rectángulo
-            // $pdf->SetLineWidth(0.2); // Establece el ancho de línea
-            // $pdf->SetDrawColor(0, 0, 0); // Establece el color del borde (negro en este caso)
-            // $pdf->Rect(105, 90, 92,10); // Dibuja un rectángulo sin relleno
-
-
-
-
- $pdf->Cell(190, 2, 'CALCULO PRIMA                                                                      BASE PARA El CALCULO', 0, 1, 'C');
-
-
-
+$pdf->Cell(190, 2, 'CALCULO PRIMA                                                                      BASE PARA El CALCULO', 0, 1, 'C');
 $pdf->SetY($pdf->GetY() +5); // Mueve hacia abajo
 
 
@@ -262,104 +232,7 @@ $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
 $pdf->Cell(40, 5, ''.$transporte.'', 1);
 $pdf->Ln(); // Salto de línea
 
-// Lista 4
-// $auxilitrans_formateado = number_format($auxilitrans, 0, ',', '.');
-// $pension_formateado = number_format($pension, 0, ',', '.');
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'AUXILIO DE TRANSPORTE', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, ''.$auxilitrans_formateado.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, 'PENSION', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, ''.$pension_formateado.'', 1);
-// $pdf->Ln(); // Salto de línea
 
-// Lista 5
-
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'HORAS EXTRA', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, '', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, 'F.S.P', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, '', 1);
-// $pdf->Ln(); // Salto de línea
-// // Lista 6
-
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'DIAS DE INCAPACIDAD', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, ''.$diasIncapacidad.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, 'DESCUENTOS', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, '', 1);
-// $pdf->Ln(); // Salto de línea
-// Lista 7
-// $pagdiasinca_formateado = number_format($pagdiasinca, 0, ',', '.');
-// // $pension_formateado = number_format($pension, 0, ',', '.');
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'PAGO DE DIAS DE INCAPACIDAD', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, ''.$pagdiasinca_formateado.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, 'PRESTAMOS', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, ''.$prestamos.'', 1);
-// $pdf->Ln(); // Salto de línea
-// Lista 8
-// $prestamos_formateado = number_format($prestamos, 0, ',', '.');
-// // $pension_formateado = number_format($pension, 0, ',', '.');
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'OTROS NO CONSTITUYE SALARIO', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, '', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, '', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, '', 1);
-// $pdf->Ln(); // Salto de línea
-
-// Lista 9
-
-// $pension_formateado = number_format($pension, 0, ',', '.');
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'DIAS VACACIONES', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, ''.$diasVacaciones.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, '', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, '', 1);
-// $pdf->Ln(); // Salto de línea
-
-// Lista 9
-// $valordiasVacaciones_formateado = number_format($valordiasVacaciones, 0, ',', '.');
-// // $pension_formateado = number_format($pension, 0, ',', '.');
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'PAGO DIAS VACACIONES', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, ''.$valordiasVacaciones_formateado.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, '', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, '', 1);
-// $pdf->Ln(); // Salto de línea
-
-// Lista 10
-// $totaldeveng_formateado = number_format($totaldeveng, 0, ',', '.');
-// $totaldeduccion_formateado = number_format($totaldeduccion, 0, ',', '.');
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(60, 5, 'AJUSTES', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(35, 5, ''.$ajustessumB.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(55, 5, 'AJUSTES', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(40, 5, ''.$ajustesresB.'', 1);
-// $pdf->Ln(); // Salto de línea
 
 // Lista 11
 $totaldeveng_formateado = number_format($totaldeveng, 0, ',', '.');
@@ -380,12 +253,10 @@ $pdf->Ln(); // Salto de línea
 
 
 
-$valorTotal=($totaldeveng+$ajustessumB)-($totaldeduccion+$ajustesresB);
+$valorTotal=$totaldeveng;
 $valorTotal_formateado = number_format($valorTotal, 0, ',', '.');
 $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
 $pdf->Cell(190, 10, 'TOTAL                                                                                                                    VALOR A PAGAR:  '.$valorTotal_formateado.'', 1);
-// $pdf->SetFont('Arial', '', 10); // Restaurar fuente normal
-// $pdf->Cell(95, 10, ' VALOR A PAGAR ', 1);
 
 
 $locale = 'es_CO'; // Define el locale para el idioma y formato de moneda colombiano
@@ -448,41 +319,14 @@ $pdf->Ln(); // Salto de línea
 
 
 
-//           // Definir borde negro alrededor del encabezado
-//           $pdf->SetLineWidth(0.5); // Establece el ancho de línea
-//           $pdf->SetDrawColor(0, 0, 0); // Establece el color del borde (negro)
-//           $pdf->Rect(10, 10, 190, 180, 'D'); // Dibuja un rectángulo con borde
-
-
-
-// // Dibuja el segundo rectángulo
-// $pdf->Ln(20);
-// $pdf->SetY($pdf->GetY() -50); // Mueve hacia abajo
-// $pdf->SetLineWidth(0.5); // Establece el ancho de línea
-// $pdf->SetDrawColor(0, 0, 0); // Establece el color del borde (negro en este caso)
-// $pdf->Rect(105, 195, 95,35); // Dibuja un rectángulo sin relleno
-
-
-
-// Elimina la palabra "coma" y lo que le sigue
-
-
 $pdf->Ln(10);
 
 $formatofin=$formato;
 
-// if ($formatofin==1){
-// 	    $pdf->MultiCell(0, 6, utf8_decode('Que '.$nombre.', identificada con la cédula de ciudadanía N.º '.$cedula.' de Bogotá, laboró en nuestra empresa desde el '.date("d",$fechadelregi).' de '.date("m",$fechadelregi).' del '.date("Y",$fechadelregi).' con un CONTRATO A TERMINO INDEFINIDO, desempeñando el cargo de '.$cargo.'.'), 0,'J');
-// }else if($formatofin==2){
-
-
-
 
 
 
 $pdf->Ln(10);
-// $pdf->MultiCell(0, 6, 'Puede certificarla veracidad de este certificado en los correos: castiblanco@bermudas.com.co y/o recursosh@bermudas.com.co 
-// ', 0,'C');
 
 $pdf->Output();
 ?>
