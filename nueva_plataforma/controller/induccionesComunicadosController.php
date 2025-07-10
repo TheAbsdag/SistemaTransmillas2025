6a0313dcea18b2fa2cf4256b9bd07d218a7f16d8
sharikug <sharikgonzalezb@gmail.com>
2025-07-10 17:24:55 -0500

    induccion y comunicados

--- nueva_plataforma/controller/induccionesComunicadosController.php
+++ nueva_plataforma/controller/induccionesComunicadosController.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once "../model/InduccionesComunicadosModel.php";
+require_once "../model/induccionesComunicadosModel.php";
 
 $modelo = new InduccionesComunicados();
 
