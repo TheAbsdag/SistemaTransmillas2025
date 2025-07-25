123026d96cf8d0857868be854aa620776cc1baeb
jarmandoR <jose523a@gmail.com>
2025-07-25 14:32:39 -0500

    wip: cambio en filtro de asignar usuario

--- resultados1.php
+++ resultados1.php
@@ -730,10 +730,10 @@ if($param1==1){
 else if($cond==27) { 
 
 	$param1=$_REQUEST["param1"]; 
-    $para=$id_sedes;
+    
 	if($param1!='Oficina'){ $cond1=" and `usu_tipovehiculo`='$param1'"; } else { $cond1=" and roles_idroles!=3"; }
 
-//  $sql="SELECT `idusuarios`,`usu_nombre`,zon_nombre FROM  seguimiento_user inner join zonatrabajo on seg_idzona=idzonatrabajo  inner join  `usuarios` on idusuarios=seg_idusuario inner join ciudades on inner_sedes=usu_idsede WHERE `roles_idroles` in (2,3,5) and seg_fechaalcohol='$fechaactual' and (usu_estado=1 or usu_filtro=1) and idciudades=$para and `seg_motivo`='Ingreso' $cond1 ";
+	$sql3="SELECT `idusuarios`,`usu_nombre`,zon_nombre FROM  seguimiento_user inner join zonatrabajo on seg_idzona=idzonatrabajo  inner join  `usuarios` on idusuarios=seg_idusuario inner join ciudades on inner_sedes=usu_idsede WHERE `roles_idroles` in (2,3,5) and seg_fechaalcohol='$fechaactual' and (usu_estado=1 or usu_filtro=1) and idciudades=$para and `seg_motivo`='Ingreso' $cond1 ";
 //	llena_datos() //$sql="SELECT `idusuarios`,`usu_nombre` FROM `usuarios` inner join ciudades on inner_sedes=usu_idsede WHERE `roles_idroles` in (2,3,5) and  (usu_estado=1 or usu_filtro=1) and idciudades=$para $cond1 ";
 	$sql = "SELECT 
     u.idusuarios,
@@ -750,10 +750,14 @@ else if($cond==27) {
     AND c.idciudades = $para
     AND su.seg_motivo = 'Ingreso'
     $cond1";
+	if ($id_usuario=523) {
+		echo$sql3;
+	}
 	echo "<select name='$nombre' id='$nombre' class='form-control' >";
 	echo "<option  value=''>Seleccione... </option>";
 	$LT->llenaselect($sql,0,"1-2", $valor, $DB);
 	echo "</select>";
+
 
 }else if($cond==28) {
 	$param1=$_REQUEST["param1"]; 
@@ -1944,13 +1948,18 @@ else if($cond==106) {
    echo "</select>";
 }
 else if($cond==108) {
-
+	$fechaactual = date("Y-m-d");
 	 $param1=$_REQUEST["param1"]; 
 	if($param1!='0'){ $cond1="and `usu_idsede`='$param1'"; } else { $cond1="and `usu_idsede`='$id_ciudad'"; }
-	echo$sql="SELECT `idusuarios`,`usu_nombre` FROM `usuarios` WHERE  (usu_estado=1 or usu_filtro=1)  $cond1 order by usu_nombre asc";
-    echo "<select name='$nombre' id='$nombre' class='form-control' >";
-	echo "<option  value='' >Seleccione... </option>";
+	// echo$sql="SELECT `idusuarios`,`usu_nombre` FROM `usuarios` WHERE  (usu_estado=1 or usu_filtro=1)  $cond1 order by usu_nombre asc";
+    $sql="SELECT `idusuarios`,`usu_nombre` FROM `usuarios` inner join `seguimiento_user` on seg_idusuario=idusuarios  WHERE `roles_idroles` in (2,3,5,6) and  (usu_estado=1 or usu_filtro=1)  $cond1 and seg_fechaalcohol='$fechaactual' ";
+    $sql1="SELECT `idusuarios`,`usu_nombre` FROM `usuarios` WHERE `roles_idroles` in (6) and (usu_estado=1 )";
+	
+	echo "<select name='$nombre' id='$nombre' class='form-control' >";
+	echo "<option  value='' >Seleccione. </option>";
 	$LT->llenaselect_ajax($sql,0,1,$para,$DB);
+	$LT->llenaselect_ajax($sql1,0,1,$para,$DB);
+
 	echo "</select>";
 }
 
