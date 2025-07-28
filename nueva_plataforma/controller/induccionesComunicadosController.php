2c724b45cf426b996b276fa37b2b12636868374e
sharikug <sharikgonzalezb@gmail.com>
2025-07-28 12:34:16 -0500

    modulo CI

--- nueva_plataforma/controller/induccionesComunicadosController.php
+++ nueva_plataforma/controller/induccionesComunicadosController.php
@@ -2,6 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once "../model/induccionesComunicadosModel.php";
 require_once __DIR__ . '/../../declara.php';
 
