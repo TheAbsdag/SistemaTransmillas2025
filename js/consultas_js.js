2070171e574607918f5e3b85fe47cf2fc0af91ae
jarmandoR <jose523a@gmail.com>
2025-08-28 18:42:32 -0500

    wip: seagrega relacion creditos al recolectar datos

--- js/consultas_js.js
+++ js/consultas_js.js
@@ -670,6 +670,13 @@ function buscarcliente(resul1)
 	var destino="resultados1.php?param1='"+tel+"'&cond="+resul1;
 	MostrarConsulta(destino, div);
 }
+function buscarcliente2(resul1)
+{
+	var div='llega_sub2';
+	var tel=document.getElementById("telefono").value;
+	var destino="resultados1.php?telefono='"+tel+"'&cond="+resul1;
+	MostrarConsulta(destino, div);
+}
 function buscarguia(resul1)
 {
 	var div='llega_sub2';
