bc9a7cc651d0b33e4e2c78472b0c7c952be96ee3
jarmandoR <jose523a@gmail.com>
2026-02-23 14:47:45 -0500

    Wip: se cambia a conexion a historico

--- connection/variables2.php
+++ connection/variables2.php
@@ -1,10 +1,18 @@
 <?php
 
 
-$bd="u713516042_transmillas2"; 
+// $bd="u713516042_transmillas2"; 
+// $host="localhost";
+// $user="u713516042_jose2";
+// $pass="Dobarli23@transmillas";
+// $Usu_ses="vive";
+// $salt = "transmi2344fsdfd";  
+
+//PRUEBAS
+$bd="u713516042_transmillas"; 
 $host="localhost";
-$user="u713516042_jose2";
-$pass="Dobarli23@transmillas";
+$user="u713516042_jose";
+$pass="0?jBMSc4GUcN";
 $Usu_ses="vive";
 $salt = "transmi2344fsdfd";  
 
