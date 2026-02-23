33558d7821524ce649a6780364edc868d0358b5b
jarmandoR <jose523a@gmail.com>
2026-02-23 14:45:31 -0500

    Wip: se cambia a conexion a historico

--- connection/variables.php
+++ connection/variables.php
@@ -8,21 +8,21 @@
 // $salt = "transmi2344fsdfd";  
 
 //PRODUCCION REMOTO
-$bd="u713516042_transmillas2"; 
-$host="45.14.88.1";
-$user="u713516042_jose2";
-$pass="Dobarli23@transmillas";
-$Usu_ses="vive";
-$salt = "transmi2344fsdfd";  
+// $bd="u713516042_transmillas2"; 
+// $host="45.14.88.1";
+// $user="u713516042_jose2";
+// $pass="Dobarli23@transmillas";
+// $Usu_ses="vive";
+// $salt = "transmi2344fsdfd";  
 
 
 //PRUEBAS
-// $bd="u713516042_transmillas"; 
-// $host="localhost";
-// $user="u713516042_jose";
-// $pass="0?jBMSc4GUcN";
-// $Usu_ses="vive";
-// $salt = "transmi2344fsdfd";  
+$bd="u713516042_transmillas"; 
+$host="localhost";
+$user="u713516042_jose";
+$pass="0?jBMSc4GUcN";
+$Usu_ses="vive";
+$salt = "transmi2344fsdfd";  
 
 
 //REMOTO PRUEBAS
