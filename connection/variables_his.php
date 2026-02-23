bf14a3dd9901de37e1015011744430cafc0d0cbd
jarmandoR <jose523a@gmail.com>
2026-02-23 14:49:22 -0500

    Wip: se cambia a conexion a historico

--- connection/variables_his.php
+++ connection/variables_his.php
@@ -21,10 +21,11 @@ $dbpass="Dobarli23@";
 $Usu_ses="vive";
 $salt = "transmi2344fsdfd";  */
 
+//PRUEBAS
 $bd="u713516042_transmillas"; 
 $host="localhost";
 $user="u713516042_jose";
-$pass="Dobarli23";
+$pass="0?jBMSc4GUcN";
 $Usu_ses="vive";
-$salt = "transmi2344fsdfd";  
+$salt = "transmi2344fsdfd";   
 ?>
